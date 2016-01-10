502a19c9a55ba7ceb2dbdd014700d6425cceaf6b
Kel Kanhirun <kel.kanhirun@gmail.com>
2016-01-10 11:35:37 -0600

    fix: replace temps with instancevars

--- features/gift_exchange_steps.rb
+++ features/gift_exchange_steps.rb
@@ -1,32 +1,32 @@
 # TODO: Rewrite as an end-to-end test
 
 Given(/^that we have Arya, Sansa, Petyr, and The Hound$/) do
-  arya  = Person.new(first_name: 'Arya')
-  sansa = Person.new(first_name: 'Sansa')
-  petyr = Person.new(first_name: 'Petyr')
-  hound = Person.new(first_name: 'The Hound')
+  @arya  = Person.new(first_name: 'Arya')
+  @sansa = Person.new(first_name: 'Sansa')
+  @petyr = Person.new(first_name: 'Petyr')
+  @hound = Person.new(first_name: 'The Hound')
 end
 
 Given(/^Arya and Sansa are sisters$/) do
   pending
-  arya.sisters << sansa
+  @arya.sisters << @sansa
 
   # guard assertion: two-way relationship
-  expect(arya.sisters).to include sansa
-  expect(sansa.sisters).to include arya
+  expect(@arya.sisters).to include @sansa
+  expect(@sansa.sisters).to include @arya
 end
 
 When(/^they do gift exchanges$/) do
   pending
-  GiftExchange.assign(arya, sansa, petyr, hound)
+  GiftExchange.assign(@arya, @sansa, @petyr, @hound)
 end
 
 Then(/^Arya should be assigned to either The Hound or Petyr$/) do
   pending
-  expect(arya.assigned_to).to eq(hound).or(petr)
+  expect(@arya.assigned_to).to eq(@hound).or(petr)
 end
 
 Then(/^Sansa should also be assigned to either The Hound or Petyr$/) do
   pending
-  expect(sansa.assigned_to).to eq(hound).or(petr)
+  expect(@sansa.assigned_to).to eq(@hound).or(petr)
 end
